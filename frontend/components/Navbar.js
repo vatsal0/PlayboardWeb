c9802b75f5afef31b1675e5713fc26c3342a7427
anthonylekan <anthonylekan25@gmail.com>
2019-06-11 09:50:54 -0400

    Navbar (v1)

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
+import styled from "styled-components";
+
+const Nav = styled.nav`
+    background: #f9c852;
+    padding: 5px;
+`;
+
+const Brand = styled.a`
+    color: #fafafa !important;
+    font-family: 'Faster One', arial;
+    font-size: 2.5em;
+    text-align: center;
+    width: 100%;
+`;
 
 class Navbar extends Component {
 
     render() {
         return (
-            <nav>
-                <a>This is the Navbar</a>
-            </nav>
+            <div>
+                <link href="https://fonts.googleapis.com/css?family=Faster+One&display=swap" rel="stylesheet"/>
+
+                <Nav className="navbar navbar-light">
+                    <Brand className="navbar-brand">
+                        Play<span style={{color: "#2274A5"}}>Board</span>
+                    </Brand>
+                </Nav>
+            </div>
         )
     }
 

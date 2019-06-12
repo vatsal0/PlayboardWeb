c64de659fd36b231b9fcecca675e1520e8068ce1
vatsal0 <34689545+vatsal0@users.noreply.github.com>
2019-06-12 14:15:48 -0400

    idek

--- frontend/components/Edit.js
+++ frontend/components/Edit.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import posed from "react-pose";
 import styled from "styled-components";
+import Animator from "../lib/Animator.js";
 
 const Background = styled.div`
     background: #F1C38E;
     width: 100vw;
-    height: 100vh;
+    height: 90vh;
     left: 0;
     top: 0;
     overflow: hidden;
@@ -15,14 +16,28 @@ const Background = styled.div`
     background-size: auto 100%;
 `
 
+const BottomBar = styled.div`
+    background: #C1C38E;
+    vertical-align: bottom;
+    height: 10vh;
+    width: 110vh;
+    margin-left: auto;
+    margin-right: auto;
+`
+
+const Dragger = posed.div({
+    draggable: "x",
+    dragBounds: {left: "0%", right: "2200%"},
+})
+
 const Player = posed.div({
     draggable: true,
     dragBounds: ({bounds}) => bounds,
 })
 
 const Container = styled.div`
-    height: 100vh;
-    width: 127vh;
+    height: 75vh;
+    width: 100vh;
     margin-left: auto;
     margin-right: auto;
     display: block;
@@ -36,6 +51,8 @@ class Edit extends Component {
         this.state = {
             playersPlaced: 0,
             players: [],
+            playerPositions: [],
+            update: undefined,
         };
     }
 
@@ -51,48 +68,67 @@ class Edit extends Component {
 
     componentWillUnmount() {
         clearInterval(this.update);
-        
     }
 
     render() {
         return (
-            <div>
+            <div style = {{background: '#F1C38E'}}>
                 <Background>
                     <Container id="Container" onClick={(e) => {
                         if (this.state.playersPlaced < 5) {
+                            let i = this.state.playersPlaced;
                             let rect = document.getElementById("Container").getBoundingClientRect();
-                            let x = (e.clientX - rect.left);
-                            let y = (e.clientY - rect.top);
-                            console.log(x,y)
+                            let ox = (e.clientX - rect.left);
+                            let oy = (e.clientY - rect.top);
                             this.setState({
                                 playersPlaced: this.state.players.push(
-                                    <Player key = {this.state.playersPlaced+1} style={{
+                                    <Player key = {i+1} style={{
                                         width: "5vh",
                                         height: "5vh",
                                         borderRadius: "10vh",
                                         position: "absolute",
-                                        left: x+"px",
-                                        top: y+"px",
+                                        left: (ox*100/rect.width)+"%",
+                                        top: (oy*100/rect.height)+"%",
                                         background: "#ff1c68",
-                                        transformOrigin: "50% 50%"
-                                    }} bounds = {{left: -x, right: rect.width - x, top: -y, bottom: rect.height - y}}>
+                                        transformOrigin: "0% 0%"
+                                    }} bounds = {{left: -ox, right: rect.width - ox, top: -oy, bottom: rect.height - oy}}
+                                    onValueChange={{
+                                        x: x => this.state.playerPositions[i].x = (x + ox - rect.left)*100/rect.width,
+                                        y: y => this.state.playerPositions[i].y = (y + oy - rect.top)*100/rect.height
+                                    }}
+                                    onDragStart={(e) => {
+                                        this.state.update = setInterval(() => {
+                                            let pos = this.state.playerPositions[i]
+                                            let frame = Animator.updateFrame(i, pos.x, pos.y);
+                                        },200/3)
+                                    }}
+                                    onDragEnd={(e) => {
+                                        clearInterval(this.state.update);
+                                    }}>
                                     </Player>
                                 )
                             })
+                            this.state.playerPositions.push({x: (ox*100/rect.width), y: (oy*100/rect.height)});
                         }
                         }}>
-                        {/* <Box style={{
-                            width: "100px",
-                            height: "100px",
-                            background: "#ff1c68",
-                            transformOrigin: "50% 50%"
-                        }}
-                        onDragEnd={e => console.log(e)}
-                        className = "Box" pose = "normal" poseKey={this.state.angle} angle = {this.state.angle}>
-                        </Box> */}
                         {this.state.players}
                     </Container>
+                    
                 </Background>
+                <BottomBar className="footer" id="bottom">
+                    <Dragger style={{
+                        width: "5vh",
+                        height: "5vh",
+                        borderRadius: "10vh",
+                        position: "absolute",
+                        left: 0,
+                        top: "92.5vh",
+                        background: "#ff1c68",
+                        transformOrigin: "50% 50%"
+                    }} bounds = {{left: "0%", right: "1000%"}}>
+
+                    </Dragger>
+                </BottomBar>
             </div>
         )
     }
--- frontend/lib/Animator.js
+++ frontend/lib/Animator.js
@@ -4,50 +4,56 @@ const PLAY_LENGTH = 24;
 
 const BallStateEnum = Object.freeze({"Holding": 1, "Passing": 2, "Shooting": 3});
 
+let frames = [];
+for(let i = 0; i < PLAY_LENGTH * FRAMES_PER_SECOND; i++) {
+    //Each frame will have an array of actions executed simultaneously when run
+    frames[i] = [];
+}
+
+let currentFrame = 0;
+
+//Initialize the state of the ball
+let ballState = BallStateEnum.Holding;
+
+//Store the last function address here for an undo feature
+let lastAction = null;
+//Arguments can be unpacked through function(...this.lastArgs)
+let lastArgs = {};
+
 class Animator {
     constructor() {
-        let frames = [];
-        for(let i = 0; i < PLAY_LENGTH * FRAMES_PER_SECOND; i++) {
-            //Each frame will have an array of actions executed simultaneously when run
-            frames[i] = [];
-        }
-        this.frames = frames;
-        this.currentFrame = 0;
-
-        //A table of Player objects whose length cannot exceed PLAYER_COUNT. Starts empty to allow for setting initial positions.
-        this.players = [];
-
-        //Initialize the state of the ball
-        this.ballState = BallStateEnum.Holding;
-
-        //Store the last function address here for an undo feature
-        this.lastAction = null;
-        //Arguments can be unpacked through function(...this.lastArgs)
-        this.lastArgs = {};
+        
     }
 
-
+    static updateFrame(playerIndex,x,y) {
+        if (currentFrame < PLAY_LENGTH * FRAMES_PER_SECOND - 1) {
+            frames[currentFrame][playerIndex] = {x:x, y:y};
+            currentFrame++;
+            return currentFrame;
+        }
+        return false;
+    }
 
     //Start running the animation at a normal rate from an optional start frame
-    play(startFrame = 0) {
+    static play(startFrame = 0) {
 
     }
 
     //Ceases playing of the animator, if it is running.
-    stop() {
+    static stop() {
 
     }
 
     //Sets the playback frame of the animation, i.e. scrolling through it
-    setFrame(frame) {this.currentFrame = frame;}
+    static setFrame(frame) {currentFrame = frame;}
 
     //Add an action (movement, pass, or shot) at the specified frame. Checks to make sure that a very similar action is not already present (i.e. a player passing the ball twice) and the action is valid (can't shoot without the ball, or pass when the ball is already in a passing state)
-    addAction(frame, action) {
+    static addAction(frame, action) {
 
     }
 
     //Delete an action in the frame given the type of action and the player performing it. Registers the call in lastAction and lastArgs (lastAction becomes addAction while the args are packed into an Action object) to allow for undoing.
-    deleteAction(frame, type, player) {
+    static deleteAction(frame, type, player) {
 
     }
 }

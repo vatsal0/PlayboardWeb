4810deaea6657c7d1fd4887e4db1e78a692c0818
ashk0821 <ashk0821@outlook.com>
2019-06-12 07:35:59 -0400

    and i oop

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -4,9 +4,6 @@ import styled from "styled-components";
 const Nav = styled.nav`
     background: #f9c852;
     padding: 5px;
-    position: absolute;
-    width: 100%;
-    height: 10%;
 `;
 
 const Brand = styled.a`
@@ -21,12 +18,18 @@ class Navbar extends Component {
 
     render() {
         return (
-            <Nav>
-                <Brand>This the Navbar</Brand>
-            </Nav>
+            <div>
+                <link href="https://fonts.googleapis.com/css?family=Faster+One&display=swap" rel="stylesheet"/>
+
+                <Nav className="navbar navbar-light">
+                    <Brand className="navbar-brand">
+                        Play<span style={{color: "#2274A5"}}>Board</span>
+                    </Brand>
+                </Nav>
+            </div>
         )
     }
 
 }
 
-export default Navbar;
+export default Navb
